4bfdf710f0cdf4b3ac1d532f9090a1e40b51203d
Kyle Lutes <klutes90@gmail.com>
2018-07-08 17:38:05 -0400

    removing unused sagas

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,20 +2,15 @@ import { History } from 'history';
 import { routerMiddleware } from 'react-router-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../data/reducers';
-import rootSaga from '../data/sagas';
 
 export default function init(history: History) {
   const initialState = {};
-  const sagaMiddleware = createSagaMiddleware();
-  const middlewares = [thunkMiddleware, sagaMiddleware, routerMiddleware(history)];
+  const middlewares = [thunkMiddleware, routerMiddleware(history)];
   const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
 
   const store = createStore(rootReducer, initialState, enhancer);
-
-  sagaMiddleware.run(rootSaga);
 
   return store;
 }
